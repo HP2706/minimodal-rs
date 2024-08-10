1370f389177f021222fe2825f353393b69dc649a
HP2706 <hprjdk@gmail.com>
2024-08-10 18:10:09 +0200

    basic hello world

---
+++ src/server/server.rs
@@ -0,0 +1,41 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use minimodal::greeter_server::{Greeter, GreeterServer};
+use minimodal::{HelloReply, HelloRequest};
+
+pub mod minimodal {
+    tonic::include_proto!("minimodal"); // The string specified here must match the proto package name
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
